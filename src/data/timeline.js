430f5fcd78583a1217e06f5287917bc61f05ec34
Ham-Choi <46688241+Ham-Choi@users.noreply.github.com>
2023-01-12 20:37:22 -0500

    Timeline Commit
    Addition of Timeline elements to website

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,3 +1,20 @@
 export default [
-    {}
+    {
+        year: 'Today',
+        title: 'TBA',
+        duration: '',
+        details: '',
+    },
+    {
+        year: 'Today',
+        title: 'TBA',
+        duration: '',
+        details: '',
+    },
+    {
+        year: 'Today',
+        title: 'TBA',
+        duration: '',
+        details: '',
+    },
 ]
