e6c7dba6f0a75406f7424970ba9b502a79faa77c
Ham-Choi <46688241+Ham-Choi@users.noreply.github.com>
2023-01-13 16:12:45 -0500

    Dark Mode
    Partial Implementation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import Title from "./Title";
 
 function Contact() {
     return (
-        <div>Contact</div>
+        <div className="flex flex-col mb-10 mx-auto">
+            <div className="flex justify-center items-center">
+                <form 
+                    action="https://getform.io/f/b0a5e528-91b5-4c58-9d38-cc2572ebb0ec"
+                    method="POST"
+                    className="flex flex-col w-full md:w-7/12"
+                    >
+                        <Title>Contact</Title>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                        />
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                        />
+                        <textarea
+                            name="message"
+                            placeholder="Message"
+                            rows="9"
+                            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                        />
+                        <button type="submit" className="text-center inline-block px-8 py-3 w-max text-base
+                        font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white">
+                            Work with me
+                        </button>
+                        
+                </form>
+            </div>
+        </div>
     )
 }
 export default Contact;
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex item-center justify-center flex-col text-center pt-20 pb-7">
-            <h1 className="text-4xl md:text-5xl mb-1 md:mb-3 font-bold">Joseph Tuong</h1>
+            <h1 className="text-4xl md:text-5xl dark:text-white mb-1 md:mb-3 font-bold">Joseph Tuong</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
             <p className="text-sm mb-6 font-bold">insert bio here</p>
         </div>
